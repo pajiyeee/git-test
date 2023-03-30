6e51f20f432e2660740021beeddee703b925c883
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-03-30 20:20:55 +0900

    Add: 메인 기능 구현 완료

--- Frontend/git-test/src/Router.js
+++ Frontend/git-test/src/Router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "./Main";
+import Main from "./pages/Main/Main";
 
 export default function Router() {
   return (
